afb5bac17d68a8ca1c376af3bb0767b0903cfa4d
aizaz.aziz <aizaz.aziz@jalebi.io>
2025-06-12 18:35:25 +0500

    initail commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/stream', function () {
+    return response()->stream(function (): void {
+        foreach (['developer', 'admin'] as $string) {
+            echo $string;
+            ob_flush();
+            flush();
+            sleep(2); // Simulate delay between chunks...
+        }
+    }, 200, ['X-Accel-Buffering' => 'no']);
+});

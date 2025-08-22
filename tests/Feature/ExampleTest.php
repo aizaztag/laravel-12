3200ef69d7e965d75a65f162be02d709911e686a
aizaz.aziz <aizaz.aziz@jalebi.io>
2025-08-22 17:11:26 +0500

    removed dd

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,9 +2,6 @@
 
 test('the application returns a successful response', function () {
     $response = $this->get('/');
-    
-    //ds('Hello LaraDumps!');      // Values and variables
-    ds($response);
 
     $response->assertStatus(200);
 });
